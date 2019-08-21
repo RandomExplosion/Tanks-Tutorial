7790c94d87589c2e33bfef91187a7ff548dca042
RandomExplosion <benjaminjmcgaw@gmail.com>
2019-08-21 18:23:34 +1000

    FInished up to phase 4 mid way through
    Fixed gitignore (I Hope)

--- Tanks/Assets/Scripts/Tank/TankMovement.cs
+++ Tanks/Assets/Scripts/Tank/TankMovement.cs
@@ -2,21 +2,21 @@
 
 public class TankMovement : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;         
-    public float m_Speed = 12f;            
-    public float m_TurnSpeed = 180f;       
-    public AudioSource m_MovementAudio;    
-    public AudioClip m_EngineIdling;       
-    public AudioClip m_EngineDriving;      
-    public float m_PitchRange = 0.2f;
+    public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
+    public float m_Speed = 12f;                 // How fast the tank moves forward and back.
+    public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
+    public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
+    public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
+    public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
+    public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
 
-    
-    private string m_MovementAxisName;     
-    private string m_TurnAxisName;         
-    private Rigidbody m_Rigidbody;         
-    private float m_MovementInputValue;    
-    private float m_TurnInputValue;        
-    private float m_OriginalPitch;         
+
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;              // The name of the input axis for turning.
+    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+    private float m_MovementInputValue;         // The current value of the movement input.
+    private float m_TurnInputValue;             // The current value of the turn input.
+    private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
 
 
     private void Awake()
@@ -25,36 +25,41 @@ public class TankMovement : MonoBehaviour
     }
 
 
-    private void OnEnable ()
+    private void OnEnable()
     {
+        // When the tank is turned on, make sure it's not kinematic.
         m_Rigidbody.isKinematic = false;
-        m_MovementInputValue = 0f;
-        m_TurnInputValue = 0f;
-        
+
+        // Also reset the input values.
         m_MovementInputValue = 0f;
         m_TurnInputValue = 0f;
     }
 
 
-    private void OnDisable ()
+    private void OnDisable()
     {
+        // When the tank is turned off, set it to kinematic so it stops moving.
         m_Rigidbody.isKinematic = true;
     }
 
 
     private void Start()
     {
+        // The axes names are based on player number.
         m_MovementAxisName = "Vertical" + m_PlayerNumber;
         m_TurnAxisName = "Horizontal" + m_PlayerNumber;
 
+        // Store the original pitch of the audio source.
         m_OriginalPitch = m_MovementAudio.pitch;
     }
-    
+
 
     private void Update()
     {
+        // Store the value of both input axes.
         m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
         m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
         EngineAudio();
     }
 
@@ -89,7 +94,7 @@ public class TankMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Move and turn the tank
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
         Move();
         Turn();
     }
