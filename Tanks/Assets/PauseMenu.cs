adff4726f633bc3fa256f8e68d5a6c7b92194cac
RandomExplosion <benjaminjmcgaw@gmail.com>
2019-09-01 14:24:19 +1000

    Finished Tutorial

---
+++ Tanks/Assets/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject m_MenuObject;
+
+
+    public void TogglePause()
+    {
+        if (Time.timeScale > 0)
+        {
+            Time.timeScale = 0; //Freeze game
+            m_MenuObject.SetActive(true); //Show the pause menu
+        }
+        else if (Time.timeScale > 0)
+        {
+            Time.timeScale = 0; //Unfreeze game
+            m_MenuObject.SetActive(false); //
+        }
+    }
+}
