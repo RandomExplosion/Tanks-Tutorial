d41ffd226e63df4971ec1e0abf9ac0b725d1f797
RandomExplosion <benjaminjmcgaw@gmail.com>
2019-09-12 15:59:22 +1000

    Finished audio section of the options menu

---
+++ Tanks/Assets/OptionsMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject m_OptionsMenu;
+    public GameObject m_MainMenu;
+    public AudioMixer mixer;
+    public Slider masterVolume;
+    public Slider sfxVolume;
+    public Slider musicVolume;
+    public Slider drivingVolume;
+
+    private void Start()
+    {
+        //Load The Volume Settings From PlayerPrefs
+        masterVolume.value = PlayerPrefs.GetFloat("masterVol");
+        sfxVolume.value = PlayerPrefs.GetFloat("sfxVol");
+        musicVolume.value = PlayerPrefs.GetFloat("musicVol");
+        drivingVolume.value = PlayerPrefs.GetFloat("masterVol");
+    }
+
+    public void SetVolumeMaster(float volume)
+    {
+        mixer.SetFloat("masterVol", volume);
+        PlayerPrefs.SetFloat("masterVol", volume);
+    }
+
+    public void SetVolumeSFX(float volume)
+    {
+        mixer.SetFloat("sfxVol", volume);
+        PlayerPrefs.SetFloat("sfxVol", volume);
+    }
+
+    public void SetVolumeMusic(float volume)
+    {
+        mixer.SetFloat("musicVol", volume);
+        PlayerPrefs.SetFloat("musicVol", volume);
+    }
+
+    public void SetVolumeDriving(float volume)
+    {
+        mixer.SetFloat("drivingVol", volume);
+        PlayerPrefs.SetFloat("drivingVol", volume);
+    }
+
+    public void SetGraphicsQuality(int qualityindex)
+    {
+        QualitySettings.SetQualityLevel(qualityindex);
+    }
+
+    public void Back()
+    {
+        m_OptionsMenu.SetActive(false);  //Turn Off the Options Menu
+        m_MainMenu.SetActive(true);    //Turn On The Main Menu
+    }
+
+}
---
+++ Tanks/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject m_OptionsMenu;
+    public GameObject m_MainMenu;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1); //Load the Game
+    }
+
+    public void Options()
+    {
+        m_OptionsMenu.SetActive(true);  //Turn On the Options Menu
+        m_MainMenu.SetActive(false);    //Turn Off The Main Menu
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
